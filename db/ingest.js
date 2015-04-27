92226b699ecf5e04263e0bdff1b55d88d0a8b5ab
Christina Holland <hsubox@gmail.com>
2015-04-27 12:36:14 -0700

    Added vowel dict.

--- db/ingest.js
+++ db/ingest.js
@@ -1,27 +1,58 @@
 var fs = require('fs');
 var Word = require('./db').Word;
 
+var VOWELS = {
+  "/&/": "a",
+  "/(@)/": "a",
+  "/A/": "a",
+  "/eI/": "a",
+  "/-/": "ir",
+  "/E/": "e",
+  "/i/": "e",
+  "/I/": "i",
+  "/aI/": "i",
+  "/Oi/": "oi",
+  "/AU/": "ow",
+  "/O/": "o",
+  "/oU/": "o",
+  "/u/": "oo",
+  "/U/": "oo",
+  "/@/": "u",
+  "/@r/": "u",
+  "\"A\"": "a",
+  "/y/": "eu",
+  "\"Y\"": "u"
+};
 
+// add word to db
+var addWord = function (word) {
+  console.log('storing word: ' + word);
+  var newWord = new Word({
+    word: word
+  });
+  newWord.save(function (err, newWord) {
+    if (err) {
+      console.log(err);
+    }
+  });
+};
+
+// go through lines in file and store words
 var storeWords = function (data) {
   var lines = data.split('\r');
   lines.forEach(function (line) {
     var wordToAdd = line.split(" ")[0].split("/")[0];
-    Word.findOne({word: wordToAdd}, function (err, word) {
-      if (err) {
-        console.log(err);
-      }
-      if (!word) {
-        console.log('storing word: ' + wordToAdd);
-        var newWord = new Word({
-          word: wordToAdd
-        });
-        newWord.save(function (err, newWord) {
-          if (err) {
-            console.log(err);
-          }
-        });
-      }
-    });
+    // Don't bother adding words with spaces in them
+    if (wordToAdd.indexOf('_') === -1) {
+      Word.findOne({word: wordToAdd}, function (err, word) {
+        if (err) {
+          console.log(err);
+        }
+        if (!word) {
+          addWord(wordToAdd);
+        }
+      });
+    }
   });
 };
 
