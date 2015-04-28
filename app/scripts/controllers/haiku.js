14c1c62ec54673e93be570dc0219e6dd178eeebd
Christina Holland <hsubox@gmail.com>
2015-04-27 20:33:37 -0700

    Receiving response data from server okay, having trouble with returning from async functions.

--- app/scripts/controllers/haiku.js
+++ app/scripts/controllers/haiku.js
@@ -12,33 +12,28 @@ angular.module('poetryPalApp')
   .controller('HaikuCtrl', function ($scope, HaikuSyllables) {
     $scope.syllablesCorrect = false;
     $scope.checkSyllables = function () {
-      if (HaikuSyllables.checkLine($scope.haikuLine1, 5) &&
-      HaikuSyllables.checkLine($scope.haikuLine2, 7) &&
-      HaikuSyllables.checkLine($scope.haikuLine3, 5)) {
+      HaikuSyllables.checkLine($scope.haikuLine1, 5)
+      .then(function () {
         console.log('checks out');
         $scope.syllablesCorrect = true;
         $scope.message = 'Everything checks out!';
-      }
+      });
     };
   })
 
   .factory('HaikuSyllables', function ($http) {
     var checkLine = function (line, expectedCount) {
-
-      $http({
+      return $http({
         url: '/api/count',
         method: 'GET',
         params: {line: line, expectedCount: expectedCount}
       })
       .then(function (res) {
-        console.log(res.data);
+        if (res.data.count === expectedCount) {
+          return true;
+        }
+        return false;
       });
-
-      // count letters, easy check to see everything else is working
-      if (line.length === expectedCount) {
-        return true;
-      }
-      return false;
     };
 
     return {
