ca2c8ce362575f16a5470054d62a703e1f057893
Christina Holland <hsubox@gmail.com>
2015-04-28 12:30:36 -0700

    Rhyme checker working on client side.

--- app/scripts/controllers/haiku.js
+++ app/scripts/controllers/haiku.js
@@ -9,58 +9,24 @@
  */
 angular.module('poetryPalApp')
 
-  .controller('HaikuCtrl', function ($scope, HaikuSyllables) {
+  .controller('HaikuCtrl', function ($scope, Syllables) {
     $scope.syllablesCorrect = false;
     $scope.haikuLine1 = 'i like maple syrup';
     $scope.haikuLine2 = 'on pancakes and on toast';
     $scope.haikuLine3 = 'but not on my eggs';
     $scope.checkSyllables = function () {
-      HaikuSyllables.checkLine($scope.haikuLine1, 5)
+      Syllables.checkLine($scope.haikuLine1, 5)
       .then(function (data) {
         $scope.haikuFeedback1 = data.message;
       });
-      HaikuSyllables.checkLine($scope.haikuLine2, 7)
+      Syllables.checkLine($scope.haikuLine2, 7)
       .then(function (data) {
-        console.log(data.message);
         $scope.haikuFeedback2 = data.message;
-
       });
-      HaikuSyllables.checkLine($scope.haikuLine3, 5)
+      Syllables.checkLine($scope.haikuLine3, 5)
       .then(function (data) {
-        if (data.message === 'OK!') {
-        }
         $scope.haikuFeedback3 = data.message;
       });
     };
-  })
-
-  .factory('HaikuSyllables', function ($http) {
-    var checkLine = function (line, expectedCount) {
-      return $http({
-        url: '/api/count',
-        method: 'GET',
-        params: {line: line, expectedCount: expectedCount}
-      })
-      .then(function (res) {
-        console.log(line);
-        console.log(res);
-        if (res.data.count === expectedCount && res.data.notFound.length === 0) {
-          return { message: 'OK!' };
-        }
-        else if (res.data.count > expectedCount) {
-          return { message: "Too many syllables. (" + res.data.count + "/" + expectedCount +")", count: res.data.count };
-        } else {
-          if (res.data.notFound.length > 0) {
-            var notFoundString = res.data.notFound.join(", ");
-            return { message: "Count indeterminate - could not find words: " + notFoundString, count: res.data.count }
-          } else {
-            return { message: "Too few syllables. (" + res.data.count + "/" + expectedCount +")", count: res.data.count };
-          }
-        }
-      });
-    };
-
-    return {
-      checkLine: checkLine
-    };
   });
+
---
+++ app/scripts/services/services.js
@@ -0,0 +1,55 @@
+'use strict';
+
+angular.module('poetryPalApp')
+
+  .factory('Syllables', function ($http) {
+    var checkLine = function (line, expectedCount) {
+      return $http({
+        url: '/api/count',
+        method: 'GET',
+        params: {line: line, expectedCount: expectedCount}
+      })
+      .then(function (res) {
+        if (res.data.count === expectedCount && res.data.notFound.length === 0) {
+          return { message: 'OK!' };
+        }
+        else if (res.data.count > expectedCount) {
+          return { message: "Too many syllables. (" + res.data.count + "/" + expectedCount +")", count: res.data.count };
+        } else {
+          if (res.data.notFound.length > 0) {
+            var notFoundString = res.data.notFound.join(", ");
+            return { message: "Count indeterminate - could not find words: " + notFoundString, count: res.data.count }
+          } else {
+            return { message: "Too few syllables. (" + res.data.count + "/" + expectedCount +")", count: res.data.count };
+          }
+        }
+      });
+    };
+
+    return {
+      checkLine: checkLine
+    };
+  })
+
+  .factory('Rhyme', function($http) {
+    var checkLines = function (line1, line2) {
+      var arr1 = line1.split(' ');
+      var arr2 = line2.split(' ');
+      var word1 = arr1[arr1.length-1];
+      var word2 = arr2[arr2.length-1];
+      return $http({
+        url: '/api/rhyme',
+        method: 'GET',
+        params: {word1: word1, word2: word2}
+      })
+      .then(function (res) {
+        console.log(word1, word2);
+        console.log(res);
+        return res.data;
+      });
+    };
+
+    return {
+      checkLines: checkLines
+    };
+  });
